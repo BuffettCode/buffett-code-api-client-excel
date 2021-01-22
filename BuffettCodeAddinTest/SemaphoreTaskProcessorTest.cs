586800dee5214200064741554fc40be3febce80b
= <=>
2021-01-22 18:49:06 +0900

    delete tests for old api latency limits

--- BuffettCodeAddinTest/SemaphoreTaskProcessorTest.cs
+++ BuffettCodeAddinTest/SemaphoreTaskProcessorTest.cs
@@ -8,42 +8,6 @@ namespace BuffettCodeAddin.Processor.UnitTests
     [TestClass]
     public class SemaphoreTaskProcessorTest
     {
-        [TestMethod]
-        public void TestProcessSequential()
-        {
-            var processor = new SemaphoreTaskProcessor<string>(1);
-            var tasks = CreateTaskList(processor, 10, 1000);
-
-            // 同時実行数1で10タスク実行
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            string[] results = Task.WhenAll(tasks).Result;
-            stopwatch.Stop();
-
-            // 約10秒かかってるはず
-            var elapsed = stopwatch.ElapsedMilliseconds;
-            System.Console.WriteLine(elapsed + " milliseconds elapsed.");
-            Assert.IsTrue(20000 > elapsed && elapsed > 8000);
-        }
-
-        [TestMethod]
-        public void TestProcessParallel()
-        {
-            var processor = new SemaphoreTaskProcessor<string>(3);
-            var tasks = CreateTaskList(processor, 10, 1000);
-
-            // 同時実行数3で10タスク実行
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            string[] results = Task.WhenAll(tasks).Result;
-            stopwatch.Stop();
-
-            // 約4秒かかってるはず
-            var elapsed = stopwatch.ElapsedMilliseconds;
-            System.Console.WriteLine(elapsed + " milliseconds elapsed.");
-            Assert.IsTrue(6000 > elapsed && elapsed > 2000);
-        }
-
         private List<Task<string>> CreateTaskList(SemaphoreTaskProcessor<string> processor, int size, int delay)
         {
             var tasks = new List<Task<string>>();
