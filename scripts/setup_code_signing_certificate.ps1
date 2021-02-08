8e99aad1dd2c6801a81c54cd9596df585f538206
Shu Suzuki <shupoppo.116@gmail.com>
2021-02-08 15:06:24 +0900

    Update scripts/setup_code_signing_certificate.ps1
    Co-authored-by: Akiomi Kamakura <akiomik@gmail.com>

--- scripts/setup_code_signing_certificate.ps1
+++ scripts/setup_code_signing_certificate.ps1
@@ -1,5 +1,5 @@
-Param([string] $S3ri, $CertificatePath, $Pass)
+Param([string] $S3Uri, $CertificatePath, $Pass)
 echo "Download an code sigining certificate to $CertificatePath from s3"
-aws s3 cp "$S3ri" "$CertificatePath" --quiet
+aws s3 cp "$S3Uri" "$CertificatePath" --quiet
 echo "Import the certificate::$CertificatePath"
 CertUtil -user -f -p "$Pass" -importpfx "$CertificatePath" NoRoot
