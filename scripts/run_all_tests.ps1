8466bfd8197ae5e46df01ef2d126f2078bd516e2
Shu Suzuki <shupoppo.116@gmail.com>
2021-02-22 11:32:17 +0900

    fix a test script

--- scripts/run_all_tests.ps1
+++ scripts/run_all_tests.ps1
@@ -1,11 +1,9 @@
 $Env:BCApiKey= "sAJGq9JH193KiwnF947v74KnDYkO7z634LWQQfPY"
 $TestTargets = (".\BuffettCodeAddinRibbonTest\bin\Release\BuffettCodeAddinRibbonTest.dll", ".\BuffettCodeIOTest\bin\Release\BuffettCodeIO.dll", ".\BuffettCodeExcelFunctionsTest\bin\Release\BuffettCodeExcelFunctionsTest.dll") 
 foreach($TestTarget in $TestTargets) {
-    if (Test-Path $TestTarget -PathType leaf) {
-        {"Test Start::$TestTarget"}
-        VSTest.Console.exe $TestTarget
-    }
-    else{
+    if (!(Test-Path $TestTarget -PathType leaf)) {
         Write-Error -Message "File $TestTarget is not found" -ErrorAction Stop
     }
 }
+
+VSTest.Console.exe[system.String]::Join(" ", $TestTarget)
