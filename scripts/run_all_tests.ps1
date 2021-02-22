1c3b03444659cd5664b1cd7a05d6a831f55f400d
Shu Suzuki <shupoppo.116@gmail.com>
2021-02-22 12:19:51 +0900

    fix tests

--- scripts/run_all_tests.ps1
+++ scripts/run_all_tests.ps1
@@ -1,9 +1,2 @@
 $Env:BCApiKey= "sAJGq9JH193KiwnF947v74KnDYkO7z634LWQQfPY"
-$TestTargets = (".\BuffettCodeAddinRibbonTest\bin\Release\BuffettCodeAddinRibbonTest.dll", ".\BuffettCodeIOTest\bin\Release\BuffettCodeIOTest.dll", ".\BuffettCodeExcelFunctionsTest\bin\Release\BuffettCodeExcelFunctionsTest.dll") 
-foreach($TestTarget in $TestTargets) {
-    if (!(Test-Path $TestTarget -PathType leaf)) {
-        throw "File $TestTarget is not found"
-    }
-}
-
-VSTest.Console.exe ($TestTargets -join " ")
+.VSTest.Console.exe \BuffettCodeAddinRibbonTest\bin\Release\BuffettCodeAddinRibbonTest.dll .\BuffettCodeIOTest\bin\Release\BuffettCodeIOTest.dll .\BuffettCodeExcelFunctionsTest\bin\Release\BuffettCodeExcelFunctionsTest.dll
