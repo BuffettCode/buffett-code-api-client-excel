90bae074792395f5a3d5b22ea3f2760fede4a7bd
Shu Suzuki <shupoppo.116@gmail.com>
2021-02-22 12:26:05 +0900

    fix test script

--- scripts/run_all_tests.ps1
+++ scripts/run_all_tests.ps1
@@ -1,2 +1,2 @@
 $Env:BCApiKey= "sAJGq9JH193KiwnF947v74KnDYkO7z634LWQQfPY"
-.VSTest.Console.exe \BuffettCodeAddinRibbonTest\bin\Release\BuffettCodeAddinRibbonTest.dll .\BuffettCodeIOTest\bin\Release\BuffettCodeIOTest.dll .\BuffettCodeExcelFunctionsTest\bin\Release\BuffettCodeExcelFunctionsTest.dll
+VSTest.Console.exe .\BuffettCodeAddinRibbonTest\bin\Release\BuffettCodeAddinRibbonTest.dll .\BuffettCodeIOTest\bin\Release\BuffettCodeIOTest.dll .\BuffettCodeExcelFunctionsTest\bin\Release\BuffettCodeExcelFunctionsTest.dll
