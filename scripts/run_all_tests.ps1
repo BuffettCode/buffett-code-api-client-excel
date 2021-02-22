c81edf4b41a81ae8f8e8141afc5210406931fb77
Shu Suzuki <shupoppo.116@gmail.com>
2021-02-22 12:15:19 +0900

    fix test

--- scripts/run_all_tests.ps1
+++ scripts/run_all_tests.ps1
@@ -1,8 +1,8 @@
 $Env:BCApiKey= "sAJGq9JH193KiwnF947v74KnDYkO7z634LWQQfPY"
-$TestTargets = (".\BuffettCodeAddinRibbonTest\bin\Release\BuffettCodeAddinRibbonTest.dll", ".\BuffettCodeIOTest\bin\Release\BuffettCodeIO.dll", ".\BuffettCodeExcelFunctionsTest\bin\Release\BuffettCodeExcelFunctionsTest.dll") 
+$TestTargets = (".\BuffettCodeAddinRibbonTest\bin\Release\BuffettCodeAddinRibbonTest.dll", ".\BuffettCodeIOTest\bin\Release\BuffettCodeIOTest.dll", ".\BuffettCodeExcelFunctionsTest\bin\Release\BuffettCodeExcelFunctionsTest.dll") 
 foreach($TestTarget in $TestTargets) {
     if (!(Test-Path $TestTarget -PathType leaf)) {
-        Write-Error -Message "File $TestTarget is not found" -ErrorAction Stop
+        throw "File $TestTarget is not found"
     }
 }
 
