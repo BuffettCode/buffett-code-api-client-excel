d954345121fe3b14e55c7dcbcc1e60a435662597
Shu Suzuki <shupoppo.116@gmail.com>
2021-07-15 10:44:09 +0900

    wip

--- scripts/sign_installer_for_build.ps1
+++ scripts/sign_installer_for_build.ps1
@@ -1,5 +1,5 @@
 Param([string] $CertificatePath)
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool"
-$InstallerPath = ".\BuffettCodeInstaller\bin\Release\ja-JP\BuffettCodeInstaller.msi"
+$InstallerPath = ".\BuffettCodeInstaller\bin\x64\Release\ja-JP\BuffettCodeInstaller.msi"
 
 Signtool sign /f $CertificatePath /t http://timestamp.comodoca.com/authenticode /v $InstallerPath
