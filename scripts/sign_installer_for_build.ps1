9c4c91017113bdbc83cfe23bea8e518423e9cac7
Shu Suzuki <shupoppo.116@gmail.com>
2021-07-17 15:54:42 +0900

    BuffettCodeInstaller => BuffettCodeExcelAddinInstaller

--- scripts/sign_installer_for_build.ps1
+++ scripts/sign_installer_for_build.ps1
@@ -1,5 +1,4 @@
-Param([string] $CertificatePath)
+Param([string] $CertificatePath, $InstallerPath)
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool"
-$InstallerPath = ".\BuffettCodeInstaller\bin\x64\Release\ja-JP\BuffettCodeInstaller.msi"
 
 Signtool sign /f $CertificatePath /t http://timestamp.comodoca.com/authenticode /v $InstallerPath
