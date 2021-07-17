19ab241ef60187cb4eff9201195d797504843a94
Shu Suzuki <shupoppo.116@gmail.com>
2021-07-17 16:31:16 +0900

    fix a script

--- scripts/upload_new_installer.ps1
+++ scripts/upload_new_installer.ps1
@@ -1,4 +1,4 @@
 Param([string] $InstallerPath, $S3Bucket, $SubFolder)
 $Folder = "buffett-code-excel-addin/$SubFolder"
-echo "upload an installer: $LocalInstaller to s3"
+echo "upload an installer: $InstallerPath to s3"
 aws s3 cp "$InstallerPath" "s3://$S3Bucket/$Folder/BuffettCodeExcelAddinInstaller.exe" --quiet 
