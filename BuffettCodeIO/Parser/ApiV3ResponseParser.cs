ea293ccf5208ea75ffc1f6ea445f0cb6769aebf7
Shu Suzuki <shupoppo.116@gmail.com>
2021-11-17 11:10:56 +0900

    refactor v3 paraser

--- BuffettCodeIO/Parser/ApiV3ResponseParser.cs
+++ BuffettCodeIO/Parser/ApiV3ResponseParser.cs
@@ -13,25 +13,14 @@ namespace BuffettCodeIO.Parser
     {
         private static IEnumerable<JToken> FindProperty(string root, JObject json) => json.Children().Where(t => t is JToken).Cast<JToken>().Where(t => t.Path.Equals(root)).ToList();
 
-        private static PropertyDictionary ParseProperties(JToken data)
+        private static PropertyDictionary ParseProperties(JToken propertiesRoot)
         {
-            var jProperties = data.Children()
+            var jProperties = propertiesRoot.Children()
             .Where(t => t is JProperty).Cast<JProperty>();
             return PropertiesParser.Parse(jProperties);
         }
 
-        private static Company ParseCompany(IEnumerable<JToken> columnDescriptions, IEnumerable<JToken> data)
-        {
-            var properties = ParseProperties(data.Children().First());
-            var descriptions = ColumnDescriptionParser.Parse(columnDescriptions.ToList());
-
-            return CreateCompany(
-                properties,
-                descriptions
-            );
-        }
-
-        private static Company CreateCompany(
+        private static Company ParseCompany(
             PropertyDictionary properties,
             PropertyDescriptionDictionary descriptions)
         {
@@ -57,14 +46,7 @@ namespace BuffettCodeIO.Parser
             );
         }
 
-        private static Quarter ParseQuarter(IEnumerable<JToken> columnDescriptions, IEnumerable<JToken> data)
-        {
-            var properties = ParseProperties(data.Children().First());
-            var descriptions = ColumnDescriptionParser.Parse(columnDescriptions.ToList());
-            return CreateQuarter(properties, descriptions);
-        }
-
-        private static Quarter CreateQuarter(
+        private static Quarter ParseQuarter(
             PropertyDictionary properties,
             PropertyDescriptionDictionary descriptions)
         {
@@ -79,16 +61,7 @@ namespace BuffettCodeIO.Parser
             );
         }
 
-        private static Daily ParseDaily(IEnumerable<JToken> columnDescriptions, IEnumerable<JToken> data)
-        {
-            var properties = ParseProperties(data.Children().First());
-            var descriptions = ColumnDescriptionParser.Parse(columnDescriptions.ToList());
-            return CreateDaily(properties, descriptions);
-        }
-
-        private static Daily CreateDaily(
-            PropertyDictionary properties,
-            PropertyDescriptionDictionary descriptions)
+        private static Daily ParseDaily(PropertyDictionary properties, PropertyDescriptionDictionary descriptions)
         {
             var period = DayPeriod.Parse(properties.Get(PropertyNames.Day));
             return Daily.Create(
@@ -107,44 +80,47 @@ namespace BuffettCodeIO.Parser
             {
                 return EmptyResource.GetInstance();
             }
-            switch (dataType)
+            else
             {
-                case DataTypeConfig.Company:
-                    return ParseCompany(columnDescriptions, data);
-                case DataTypeConfig.Quarter:
-                    return ParseQuarter(columnDescriptions, data);
-                case DataTypeConfig.Daily:
-                    return ParseDaily(columnDescriptions, data);
-                default:
-                    throw new NotSupportedDataTypeException($"Parse {dataType} is not supported at V3");
+                var descriptions = ColumnDescriptionParser.Parse(columnDescriptions.ToList());
+                var properties = ParseProperties(data.Children().First());
+                switch (dataType)
+                {
+                    case DataTypeConfig.Company:
+                        return ParseCompany(properties, descriptions);
+                    case DataTypeConfig.Quarter:
+                        return ParseQuarter(properties, descriptions);
+                    case DataTypeConfig.Daily:
+                        return ParseDaily(properties, descriptions);
+                    default:
+                        throw new NotSupportedDataTypeException($"Parse {dataType} is not supported at V3");
+                }
             }
+
         }
 
-        private static IList<Quarter> ParseBulkQuarter(IEnumerable<JToken> columnDescriptions, IEnumerable<JToken> data)
-        {
-            var descriptions = ColumnDescriptionParser.Parse(columnDescriptions.ToList());
-
-            return data.Children().Children().First().Select(d => CreateQuarter(ParseProperties(d), descriptions)).ToList();
-        }
 
         public IList<IApiResource> ParseRange(DataTypeConfig dataType, JObject json)
         {
             var columnDescriptions = FindProperty(PropertyNames.ColumnDescription, json);
             var data = FindProperty(PropertyNames.Data, json);
+
             if (data.Count() == 0)
             {
                 // return Empty 
                 return new List<IApiResource>().ToList();
             }
-            switch (dataType)
+            else
             {
-                case DataTypeConfig.Quarter:
-                    return ParseBulkQuarter(columnDescriptions, data).Cast<IApiResource>().ToList();
-                default:
-                    throw new NotSupportedDataTypeException($"Parse {dataType} is not supported at V3");
+                var descriptions = ColumnDescriptionParser.Parse(columnDescriptions.ToList());
+                switch (dataType)
+                {
+                    case DataTypeConfig.Quarter:
+                        return data.Children().Children().First().Select(d => ParseQuarter(ParseProperties(d), descriptions)).Cast<IApiResource>().ToList();
+                    default:
+                        throw new NotSupportedDataTypeException($"Parse {dataType} is not supported at V3");
+                }
             }
         }
-
-
     }
 }
