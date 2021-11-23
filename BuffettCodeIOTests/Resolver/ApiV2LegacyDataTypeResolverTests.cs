c008f927bed2553e045384f749fe6cbe7688cae3
Shu Suzuki <shupoppo.116@gmail.com>
2021-11-23 12:35:22 +0900

    fix test

--- BuffettCodeIOTests/Resolver/ApiV2LegacyDataTypeResolverTests.cs
+++ BuffettCodeIOTests/Resolver/ApiV2LegacyDataTypeResolverTests.cs
@@ -9,13 +9,16 @@ namespace BuffettCodeIO.Resolver.Tests
         [TestMethod]
         public void TestResolve()
         {
-            var resolver = Resolver.ApiV2LegacyDataTypeResolver.GetInstance();
+            var resolver = ApiV2LegacyDataTypeResolver.GetInstance();
 
-            var quarter = resolver.Resolve("net_sales");
-            Assert.AreEqual(quarter, DataTypeConfig.Quarter);
+            // quarter にしかないもの
+            Assert.AreEqual(resolver.Resolve("net_sales"), DataTypeConfig.Quarter);
 
-            var indicator = resolver.Resolve("roe");
-            Assert.AreEqual(indicator, DataTypeConfig.Indicator);
+            // quarter, indicator 両方にあるものは quarter を優先する
+            Assert.AreEqual(resolver.Resolve("roe"), DataTypeConfig.Quarter);
+
+            // indicator のみにあるもの
+            Assert.AreEqual(resolver.Resolve("eps_forecast"), DataTypeConfig.Indicator);
 
             // 実装上、未知の項目名はQuarter扱いになる
             var others = resolver.Resolve("unknown_property");
