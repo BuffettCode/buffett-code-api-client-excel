d9b050cbe36a37910c1d6778d4d5557abcdb40fb
Shu Suzuki <shupoppo.116@gmail.com>
2022-01-15 11:27:59 +0900

    remove an old function

--- BuffettCodeAddinRibbon/CsvDownloadForm.cs
+++ BuffettCodeAddinRibbon/CsvDownloadForm.cs
@@ -5,7 +5,6 @@ using BuffettCodeCommon;
 using BuffettCodeCommon.Config;
 using BuffettCodeCommon.Exception;
 using BuffettCodeCommon.Period;
-using BuffettCodeCommon.Validator;
 using BuffettCodeIO.Property;
 using BuffettCodeIO.TabularOutput;
 using System;
@@ -172,37 +171,6 @@ namespace BuffettCodeAddinRibbon
         private void TextTo_Validated(object sender, EventArgs e)
         {
             errorProvider.SetError(textTo, "");
-        }
-
-        private string ValidateFiscalQuarter(string param)
-        {
-            var tokens = param.Split('Q');
-            if (tokens.Length != 2)
-            {
-                return "フォーマットが正しくありません。(例: 2017Q1)";
-            }
-            else if (!uint.TryParse(tokens[0], out uint year))
-            {
-                return "年が数値ではありません。";
-            }
-            else if (!uint.TryParse(tokens[1], out uint quarter))
-            {
-                return "四半期が数値ではありません。";
-            }
-            else
-            {
-                try
-                {
-                    FiscalYearValidator.Validate(year);
-                }
-                catch (ValidationError) { return $"年の設定が不正です: {year}"; }
-                try
-                {
-                    FiscalQuarterValidator.Validate(quarter);
-                }
-                catch (ValidationError) { return $"四半期の設定が不正です: {quarter}"; }
-            }
-            return null;
         }
     }
 }
