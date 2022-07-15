5e1bdb468fa0739610c9f80813a4116858f28202
Shu Suzuki <shupoppo.116@gmail.com>
2022-07-15 17:40:11 +0900

    mod setting
    update test pfx file

--- scripts/sign_installer_for_build.ps1
+++ scripts/sign_installer_for_build.ps1
@@ -1,4 +1,6 @@
 Param([string] $CertificatePath, $InstallerPath)
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool"
 
-Signtool sign /f $CertificatePath /t http://timestamp.comodoca.com/authenticode /v $InstallerPath
+Signtool sign /debug /f $CertificatePath /t http://timestamp.comodoca.com/authenticode /v $InstallerPath
+
+#Signtool sign /debug /f $CertificatePath /t http://timestamp.comodoca.com/authenticode /v $InstallerPath
