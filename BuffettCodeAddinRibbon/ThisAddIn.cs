4387ba246ddb2c631bc340db70882ecc4e0cfa9f
Shu Suzuki <shupoppo.116@gmail.com>
2022-07-15 16:41:04 +0900

    version up .NET version to 4.6.2
    comment out useless settings
    
    fix csproj
    
    mod lint command
    
    use .NET 4.6.2

--- BuffettCodeAddinRibbon/ThisAddIn.cs
+++ BuffettCodeAddinRibbon/ThisAddIn.cs
@@ -9,19 +9,5 @@ namespace BuffettCodeAddinRibbon
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
         }
-
-        #region VSTO で生成されたコード
-
-        /// <summary>
-        /// デザイナーのサポートに必要なメソッドです。
-        /// コード エディターで変更しないでください。
-        /// </summary>
-        private void InternalStartup()
-        {
-            this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
-        }
-
-        #endregion
     }
 }
