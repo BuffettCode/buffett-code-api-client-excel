aad3c279d057dcf240f1633ee0bce69c4cc9e88f
Shu Suzuki <shupoppo.116@gmail.com>
2022-07-17 16:36:11 +0900

    mod build scripts

--- scripts/sign_installer_for_build.ps1
+++ scripts/sign_installer_for_build.ps1
@@ -1,4 +1,4 @@
 Param([string] $CertificatePath, $InstallerPath)
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool"
 
-Signtool sign /f $CertificatePath /t http://timestamp.comodoca.com/authenticode /v $InstallerPath
+Signtool sign /debug /f $CertificatePath /t http://timestamp.comodoca.com/authenticode /v $InstallerPath
